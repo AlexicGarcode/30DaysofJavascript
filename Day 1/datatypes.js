1c2f14a94e72cc3d17cf80c73b32ef30a0bba89c
unknown <garcovicente@gmail.com>
2022-01-01 18:30:42 -0600

    Day1

---
+++ Day 1/datatypes.js
@@ -0,0 +1,35 @@
+let stringo = "pato";
+let booleano = false;
+let undefinido = undefined;
+let nullo = null;
+let numbero = 5;
+
+console.log(typeof stringo);
+console.log(typeof booleano);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof numbero);
+
+
+let variable1 = null;
+let variable2 = null;
+let variable3 = null;
+let variable4 = null;
+
+let variable5 = 'Gato';
+let variable6 = 'Perro';
+let variable7 = 'Tiburon';
+let variable8 =  'Cabra';
+
+
+let firstName = 'Vicente';
+let lastName = 'Garcia';
+let maritalStatus = 'married';
+let contry = 'Mexico';
+let age = '33';
+
+let myAge = 'I am 25 years old.';
+let yourAge = 'You are 30 years old.';
+
+console.log(myAge);
+console.log(yourAge);
---
+++ Day 1/variable.js
@@ -0,0 +1,12 @@
+//Comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+
+let stringo = "pato";
+let booleano = false;
+let undefinido = undefined;
+let nullo = null;
